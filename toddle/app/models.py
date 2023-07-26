a6543e57b92d9cde2a1bd078bc7399212144c648
Axel Tokam <mail.axeltokam@gmail.com>
2023-07-26 21:43:04 +0100

    Created a basic Task model

--- toddle/app/models.py
+++ toddle/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TaskModel(models.Model):
+      title = models.CharField(max_length=150, null=False, blank=False)
+      description = models.TextField(max_length=500, null=True, blank=True)
+      created_at = models.DateTimeField()
+
+      def __str__(self):
+            return self.title
